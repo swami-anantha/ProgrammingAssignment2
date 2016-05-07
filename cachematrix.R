988ae904f56e9a79c0032d310128a66f38bfad18
swananth <swananth@cisco.com>
2016-05-07 11:54:18 -0700

    Updated with the assignment code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## Use the makeCacheMatrix function to create a cached inverse matrix object
+## Use the cacheSolve function to get the inverse matrix from the cached inverse matrix object
+##     When the cacheSolve function is run for the first time, it will calculate and
+##     cache the inverse matrix value in the cached inverse matrix object
+##     This cached inverse matrix value will be used the next time the cacheSolve function is called
+##     with the relevant cached inverse matrix object
+##
 
-## Write a short comment describing this function
 
+#
+# makeCacheMatrix Function:
+#    Builds a cached inverse matrix object for the matrix
+#        that was passed in as argument
+#    Returns the cached inverse matrix object
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseVal <- NULL
+    set <- function(y) {
+        x <<- y
+        inverseVal <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(invVal) inverseVal <<- invVal
+    getInverse <- function() inverseVal
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+#
+# cacheSolve Function:
+#    Calculates the inverse matrix value for the cached object
+#        that is passed in as argument
+#    Does not run the inverse matrix calculation if
+#        the cached object has the cached inverse value
+#    Returns the inverse matrix value
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseVal <- x$getInverse()
+    if(!is.null(inverseVal)) {
+        message("Getting cached inverse matrix value")
+        return(inverseVal)
+    }
+    data <- x$get()
+    inverseVal <- solve(data, ...)
+    x$setInverse(inverseVal)
+    inverseVal
 }
+
